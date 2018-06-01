ecf79aaa3061a0bbd1b04b1cfd37b078ad291954
protatodev <thaddonaghue@yahoo.com>
2018-06-01 11:58:13 -0700

    add preliminary javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
 
+  $("#submit").click(function() {
+    $(".pre-hidden").show();
+  });
+
+  $("#reset").click(function() {
+    $("input").attr("disabled", false);
+    $("span").text("");
+    $(".pre-hidden").hide();
+  });
+
   $("input").click(function() {
     $(this).attr("disabled", true);
   });
